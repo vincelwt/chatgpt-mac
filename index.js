d2b03166a114711c65a89fe124942a8e42ae8c7d
Sam Asante <sam@vissol.co.uk>
2022-12-05 18:49:03 +0000

    Set better default window size

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ app.on("ready", () => {
         webviewTag: true,
         // nativeWindowOpen: true,
       },
-      width: 450,
-      height: 550,
+      width: 500,
+      height: 650,
     },
     tray,
     showOnAllWorkspaces: true,
@@ -48,9 +48,7 @@ app.on("ready", () => {
 
   mb.on("ready", () => {
     const { window } = mb;
-
     window.setAlwaysOnTop(true, "floating", 1);
-
     if (process.platform !== "darwin") {
       window.setSkipTaskbar(true);
     } else {
