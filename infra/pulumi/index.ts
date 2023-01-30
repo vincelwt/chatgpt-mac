dfc91abf63614298655daffde0cc6060be4fd93e
Igor Shapiro <shapigor@gmail.com>
2023-01-30 11:17:44 +0200

    feat(s3): make bucket public writable

--- infra/pulumi/index.ts
+++
@@ -1,6 +0,0 @@
-export * from '@lemonade/infra-common/deployment/shared'
-import { ObjectStorage } from "@lemonade/infra-common/deployment";
-
-new ObjectStorage("gpt-project", {
-  name: "gpt-project",
-});
